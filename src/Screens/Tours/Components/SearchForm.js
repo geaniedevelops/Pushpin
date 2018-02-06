776b6e7379b69e8ec5d844af8ce3fea09d937107
geanie blanco <geanieblanco@gmail.com>
2018-02-06 00:39:50 -0500

    Ballmer Peak - ios build success

--- src/Screens/Tours/Components/SearchForm.js
+++ src/Screens/Tours/Components/SearchForm.js
@@ -1,57 +1,53 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet } from "react-native";
-import { Container, Header, Title, Content, Button, Text, Right, Body, Left, Picker, Form, Item, Item as FormItem , Input} from "native-base";
-const Options = Picker.Item;
-
-export default class SearchForm extends Component {
+import React, { Component } from "react";
+import { Platform } from "react-native";
+import { Container, Header, Title, Content, Button, Icon, Text, Right, Body, Left, Picker, Form, Item as FormItem } from "native-base";
+const Item = Picker.Item;
+export default class PickerExample extends Component {
   constructor(props) {
-      super(props);
-      this.state = {
-        selected3: "key"
-      };
-    }
-    onValueChange3(value: string) {
-      this.setState({
-        selected3: value
-      });
-    }
-
+    super(props);
+    this.state = {
+      selected1: "key1"
+    };
+  }
+  onValueChange(value: string) {
+    this.setState({
+      selected1: value
+    });
+  }
   render() {
     return (
-      <Container
-        style={styles.container}>
-        <Content>
-          <Item>
-            <Input placeholder="Zip Code" />
-          </Item>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent onPress={() => this.props.navigation.goBack()}>
+              <Icon name="arrow-back" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Picker</Title>
+          </Body>
+          <Right />
+        </Header>
         <Content>
           <Form>
             <Picker
+              iosHeader="Select one"
               mode="dropdown"
-              headerBackButtonText="Baaack!"
-              selectedValue={this.state.selected3}
-              onValueChange={this.onValueChange3.bind(this)}
+              selectedValue={this.state.selected1}
+              onValueChange={this.onValueChange.bind(this)}
             >
-              <Options label="Select A Theme" value="key" />
-              <Options label="History" value="History" />
-              <Options label="Art" value="Art" />
-              <Options label="Fashion" value="Fashion" />
-              <Options label="Music" value="Music" />
-              <Options label="Finance" value="Finance" />
-              <Options label="Crime/Ghost" value="Gritty" />
-              <Options label="Food & Drink" value="Foodie" />
+            <Item label="Select A Theme" value="key" />
+            <Item label="History" value="History" />
+            <Item label="Art" value="Art" />
+            <Item label="Fashion" value="Fashion" />
+            <Item label="Music" value="Music" />
+            <Item label="Finance" value="Finance" />
+            <Item label="Crime/Ghost" value="Gritty" />
+            <Item label="Food & Drink" value="Foodie" />
             </Picker>
           </Form>
-        </Content>
         </Content>
       </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#FEFAEC",
-    height: 200
-  }
-})
